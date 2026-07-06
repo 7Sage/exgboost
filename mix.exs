91edcf5da995f77865946a2d105b1f4aca99fcef
Ian Perks <ian@adaptcc.co.uk>
2026-07-06 08:27:18 +0100

    ex_json_schema upgrade

--- mix.exs
+++ mix.exs
@@ -53,7 +53,7 @@ defmodule EXGBoost.MixProject do
       {:ex_doc, "~> 0.40", only: :docs},
       {:cc_precompiler, "~> 0.1.0", runtime: false},
       {:exterval, "0.2.0"},
-      {:ex_json_schema, "~> 0.11.0"},
+      {:ex_json_schema, "~> 0.11.4"},
       {:vega_lite, "~> 0.1"},
       {:vega_lite_convert, "~> 1.0.1"},
       {:kino, "~> 0.11"},
