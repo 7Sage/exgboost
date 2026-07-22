bf5d2f34ca15dc4dfc3194ebcf36aa172bf8a840
GitHub Action <action@github.com>
2026-07-22 22:20:36 +0000

    Add checksum.exs for v0.9.2

--- checksum.exs
+++ checksum.exs
@@ -1,9 +1,9 @@
 %{
-  "exgboost-nif-2.17-aarch64-linux-gnu-0.9.1.tar.gz" => "sha256:9e91e51629942789eaa00308ccc370479afb19dd51d45794b1224884047b33f7",
-  "exgboost-nif-2.17-riscv64-linux-gnu-0.9.1.tar.gz" => "sha256:383b29b361cc6bc615e68328e576c2e2e95a9ac24a961b6d04989b768f071b1c",
-  "exgboost-nif-2.17-x86_64-linux-gnu-0.9.1.tar.gz" => "sha256:5287c30b05cffe04921200083416f1a923630870ebd86d5080ad2a8c9107ed02",
-  "exgboost-nif-2.18-aarch64-apple-darwin-0.9.1.tar.gz" => "sha256:bfc1c7b075856d2dc4b153985243134ec77350f625ffb58932f55ae3a52850d5",
-  "exgboost-nif-2.18-aarch64-linux-gnu-0.9.1.tar.gz" => "sha256:bfd16986ba7837ce63d721ee077448add14588cc88fe43b0359b7d7b694cf3f2",
-  "exgboost-nif-2.18-riscv64-linux-gnu-0.9.1.tar.gz" => "sha256:998a90e22d88028506c54cb64df1e6824704264ff0accee18548dc4a9645f79e",
-  "exgboost-nif-2.18-x86_64-linux-gnu-0.9.1.tar.gz" => "sha256:c3749a476347fdd43603bb30115b7147ed29c0c54321f730530a2153972a9973",
+  "exgboost-nif-2.17-aarch64-linux-gnu-0.9.2.tar.gz" => "sha256:56d714af2a21fbe74e193810d761bf9f82bee39da6d05fdacdf81e63b26e0efc",
+  "exgboost-nif-2.17-riscv64-linux-gnu-0.9.2.tar.gz" => "sha256:86372485a79f5fa73120775185a4ce815064ad2ccc60928d5c1ba84f23ab46cf",
+  "exgboost-nif-2.17-x86_64-linux-gnu-0.9.2.tar.gz" => "sha256:43e92ba39f87f2bdc8f5162ad4c8873939d76ea91d17f66a3bfa762632892d73",
+  "exgboost-nif-2.18-aarch64-apple-darwin-0.9.2.tar.gz" => "sha256:78740cb552096c52759d6b101977dfbe749df2cb6c2e67c828b86bc845ad75e2",
+  "exgboost-nif-2.18-aarch64-linux-gnu-0.9.2.tar.gz" => "sha256:2404d3d0f981dc5b21615882cdf9eb6d9d4736cfa22c179adea088e3950c7f66",
+  "exgboost-nif-2.18-riscv64-linux-gnu-0.9.2.tar.gz" => "sha256:cf1ff62384778832a8c950b5864a053292b36f41c953bf80dfde22a52039a1e5",
+  "exgboost-nif-2.18-x86_64-linux-gnu-0.9.2.tar.gz" => "sha256:d27e863d8a4c5b49c18ff396dcd72833aa0af98d9cecceeda14cb88688891591",
 }
