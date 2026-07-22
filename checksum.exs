83cb4242750b0b63ca0ddc06d6c6bab5fc814f8e
Pedro Perrone <psperrone@hotmail.com>
2026-07-22 18:16:54 -0300

    Add checksum

---
+++ checksum.exs
@@ -0,0 +1,9 @@
+%{
+  "exgboost-nif-2.17-aarch64-linux-gnu-0.9.1.tar.gz" => "sha256:9e91e51629942789eaa00308ccc370479afb19dd51d45794b1224884047b33f7",
+  "exgboost-nif-2.17-riscv64-linux-gnu-0.9.1.tar.gz" => "sha256:383b29b361cc6bc615e68328e576c2e2e95a9ac24a961b6d04989b768f071b1c",
+  "exgboost-nif-2.17-x86_64-linux-gnu-0.9.1.tar.gz" => "sha256:5287c30b05cffe04921200083416f1a923630870ebd86d5080ad2a8c9107ed02",
+  "exgboost-nif-2.18-aarch64-apple-darwin-0.9.1.tar.gz" => "sha256:bfc1c7b075856d2dc4b153985243134ec77350f625ffb58932f55ae3a52850d5",
+  "exgboost-nif-2.18-aarch64-linux-gnu-0.9.1.tar.gz" => "sha256:bfd16986ba7837ce63d721ee077448add14588cc88fe43b0359b7d7b694cf3f2",
+  "exgboost-nif-2.18-riscv64-linux-gnu-0.9.1.tar.gz" => "sha256:998a90e22d88028506c54cb64df1e6824704264ff0accee18548dc4a9645f79e",
+  "exgboost-nif-2.18-x86_64-linux-gnu-0.9.1.tar.gz" => "sha256:c3749a476347fdd43603bb30115b7147ed29c0c54321f730530a2153972a9973",
+}
